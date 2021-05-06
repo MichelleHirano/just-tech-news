ca07a53e443dc7349661a8505c2c19dee12838b5
MHirano <michelle@hiranohomes.com>
2021-05-06 13:49:30 -0700

    add comments

---
+++ models/Comment.js
@@ -0,0 +1,47 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+              }
+        },
+        comment_test:{
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+              }
+        },
+        post_id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'post',
+              key: 'id'
+            }
+        }
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+
+module.exports = { User, Post, Vote, Comment };
